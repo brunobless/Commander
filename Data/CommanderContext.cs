724b878c5eea123bf3ebd38d9bab69015eb0c9ef
Bruno Bless <bbless@proffix.net>
2020-06-08 14:42:26 +0200

    Added DBContext, ConnectionStrings and updated Models for Entity Framework Core implementation

---
+++ Data/CommanderContext.cs
@@ -0,0 +1,22 @@
+﻿using Commander.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace Commander.Data
+{
+    public class CommanderContext : DbContext
+    {
+        // Constructor
+        public CommanderContext(DbContextOptions<CommanderContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Command> Commands { get; set; }
+
+    }
+}
--- Models/Command.cs
+++ Models/Command.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
@@ -8,9 +9,17 @@ namespace Commander.Models
 {
     public class Command
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(250)]
         public string HowTo { get; set; }
+
+        [Required]
         public string Line { get; set; }
+
+        [Required]
         public string Plattform { get; set; }
     }
 }
